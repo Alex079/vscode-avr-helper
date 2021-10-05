951bdf8a8d63a01919d90732ef592c45760e2420
alex079 <>
2021-10-05 23:54:27 +0200

    fix device type pre-select

--- src/main/actions/SetupTools.ts
+++ src/main/actions/SetupTools.ts
@@ -36,9 +36,10 @@ export async function setupTools(): Promise<void> {
 export async function setupDevice(): Promise<void> {
   const folder = await pickFolder();
   const devTypes = await listTypes(folder.uri, '-p?');
+  const cut = (s: string): string => s.split(/\s+/, 1)[0].toLowerCase();
 
-  pickOne('Device type', devTypes, item => item.label.toLowerCase() === C.DEVICE_TYPE.get(folder.uri), 1, 2)
-    .then(newDevType => newDevType.label.split(/\s+/, 1)[0].toLowerCase())
+  pickOne('Device type', devTypes, item => cut(item.label) === C.DEVICE_TYPE.get(folder.uri), 1, 2)
+    .then(newDevType => cut(newDevType.label))
     .then(newDevType => C.DEVICE_TYPE.set(folder.uri, newDevType))
     .then(() => pickNumber('Device frequency', C.DEVICE_FREQ.get(folder.uri), true, 2, 2))
     .then(newFrequency => C.DEVICE_FREQ.set(folder.uri, newFrequency))
